31e5360458acf298f79a2dd8a0d38015321ec5e6
YKJ <rudwns7552@gmail.com>
2023-02-13 08:01:51 +0900

    - 격자판회문수   # 강의     1. 배열 slice 비교를 통해 row Check     2. 3중 loop를 통해 col Check

--- section3/격자판회문수.py
+++ section3/격자판회문수.py
@@ -16,24 +16,39 @@ sys.stdin = open("격자판회문수.txt","rt")
 
 lst = [list(map(int,input().split())) for _ in range(7)]
 
-rowCnt = 0
-colCnt = 0
-e = 4
+# 내 방식
+# rowCnt = 0
+# colCnt = 0
+# e = 4
+# sum = 0
+# for i in range(7):
+#     for j in range(3):
+#         rowCnt = 0
+#         colCnt = 0
+#         e = 4
+#         for k in range(2):
+#             if lst[i][j+k] == lst[i][j+e]:
+#                 rowCnt += 1
+#             if lst[j+k][i] == lst[j+e][i]:
+#                 colCnt += 1
+#             e -= 1
+#         if rowCnt == 2:
+#             sum += 1
+#         if colCnt == 2:
+#             sum += 1
+
+# 강의
 sum = 0
-for i in range(7):
-    for j in range(3):
-        rowCnt = 0
-        colCnt = 0
-        e = 4
-        for k in range(2):
-            if lst[i][j+k] == lst[i][j+e]:
-                rowCnt += 1
-            if lst[j+k][i] == lst[j+e][i]:
-                colCnt += 1
-            e -= 1
-        if rowCnt == 2:
+for i in range(3):
+    for j in range(7):
+        tmp = lst[j][i:i+5]
+        if tmp == tmp[::-1]: #row check
             sum += 1
-        if colCnt == 2:
+
+        for k in range(2):#col check
+            if lst[i+k][j] != lst[i+4-k][j]:
+                break
+        else:
             sum += 1
 
 print(sum)
