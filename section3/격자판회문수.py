032e53bfade5b514209436da9f57895f6ff27900
YKJ <rudwns7552@gmail.com>
2023-02-13 01:25:19 +0900

    - 격자판회문수   # 내 방식     1. 1중 loop에서는 각 행열 만큼 loop     2. 2중 loop에서는 배열 idx는 0~2 까지만 확인하면 됨        >> 그 이상의 비교는 무의미     3. 3중 loop에서는 각각 2번씩 비교 할 수 있도록 반복문 처리        >> 0 & 4 / 1 & 3           1 & 5 / 2 & 4           2 & 6 / 3 & 5

---
+++ section3/격자판회문수.py
@@ -0,0 +1,39 @@
+# 1부터 9까지의 자연수로 채워진 7*7 격자판이 주어지면 격자판 위에서 가로방향 또는 세로방향으로
+# 길이 5자리 회문수가 몇 개 있는지 구하는 프로그램을 작성하세요.
+#
+# 회문수란 121과 같이 앞에서부터 읽으나 뒤에서부터 읽으나 같은 수를 말합니다.
+# 빨간색처럼 구부러진 경우(87178)는 회문수로 간주하지 않습니다.
+#
+# ▣ 입력설명
+# 1부터 9까지의 자연수로 채워진 7*7격자판이 주어집니다.
+#
+# ▣ 출력설명
+# 5자리 회문수의 개수를 출력합니다.
+
+import sys
+
+sys.stdin = open("격자판회문수.txt","rt")
+
+lst = [list(map(int,input().split())) for _ in range(7)]
+
+rowCnt = 0
+colCnt = 0
+e = 4
+sum = 0
+for i in range(7):
+    for j in range(3):
+        rowCnt = 0
+        colCnt = 0
+        e = 4
+        for k in range(2):
+            if lst[i][j+k] == lst[i][j+e]:
+                rowCnt += 1
+            if lst[j+k][i] == lst[j+e][i]:
+                colCnt += 1
+            e -= 1
+        if rowCnt == 2:
+            sum += 1
+        if colCnt == 2:
+            sum += 1
+
+print(sum)
