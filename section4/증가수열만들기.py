2b0eac378515ff1a11df9333f47b276659dacc97
YKJ <rudwns7552@gmail.com>
2023-03-27 00:38:07 +0900

    - 증가수열만들기(그리디)   1) list > deque 선언      - list는 pop연산 시 인덱스 새로 잡는 연산이 일어남.      - deque는 pop연산 시 인덱스 연산 없이 추가/삭제 가능   2) 배열 loop 실행   3) 선후 절대값 구해서 어느 값이 마지막 절대 값과 가까운지 target 설정   4) 타겟이 증가수열에 유효하면      - popLeft 또는 pop 연산 수행   5) 유효하지 않으면 루프 종료

---
+++ section4/증가수열만들기.py
@@ -0,0 +1,55 @@
+# 1부터 N까지의 모든 자연수로 구성된 길이 N의 수열이 주어집니다.
+# 이 수열의 왼쪽 맨 끝 숫자 또는 오른쪽 맨 끝 숫자 중 하나를 가져와 나열하여 가장 긴 증가수열을 만듭니다.
+#
+# 이 때 수열에서 가져온 숫자(왼쪽 맨 끝 또는 오른쪽 맨 끝)는 그 수열에서 제거됩니다.
+# 예를 들어 2 4 5 1 3 이 주어지면 만들 수 있는 가장 긴 증가수열의 길이는 4입니다.
+#
+# 맨 처음 왼쪽 끝에서 2를 가져오고, 그 다음 오른쪽 끝에서 3을 가져오고, 왼쪽 끝에서 4,
+# 왼쪽 끝에서 5를 가져와 2 3 4 5 증가수열을 만들 수 있습니다.
+
+# ▣ 입력설명
+# 첫째 줄에 자연수 N(3<=N<=100)이 주어집니다.
+# 두 번째 줄에 N개로 구성된 수열이 주어집니다.
+
+# ▣ 출력설명
+# 첫째 줄에 최대 증가수열의 길이를 출력합니다.
+# 두 번째 줄에 가져간 순서대로 왼쪽 끝에서 가져갔으면 'L', 오른쪽 끝에서 가져갔으면 'R'를 써 간 문자열을 출력합니다.
+# (단 마지막에 남은 값은 왼쪽 끝으로 생각합니다.)
+
+import sys
+from collections import deque
+
+sys.stdin = open("증가수열만들기.txt","rt")
+
+N = int(input())
+lst = list(map(int, input().split()))
+lst = deque(lst)
+
+str = ''
+cnt = 0
+lastNum = 0
+
+while lst:
+    first = lst[0]
+    last = lst[len(lst)-1]
+    fAbs = abs(lastNum - first)
+    lAbs = abs(lastNum - last)
+
+    target = first
+    if fAbs > lAbs:
+        target = last
+
+    if lastNum < target:
+        if target == first:
+            str += 'L'
+            cnt += 1
+            lastNum = lst.popleft()
+        else:
+            str += 'R'
+            cnt += 1
+            lastNum = lst.pop()
+    else:
+        break
+
+print(cnt)
+print(str)
